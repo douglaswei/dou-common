90d07f46e625877403c8933aa6843e8a725ac706
douglaswei <kkoyyo@gmail.com>
2021-09-27 00:57:24 +0800

    Update config.go

--- consul-config/config.go
+++ consul-config/config.go
@@ -19,6 +19,7 @@ func GetConfig() *config.Config {
 
 func GetConfigByAddress(consulAddress string) *config.Config {
     config.AddDriver(yaml.Driver)
+    
     dftCfg := config.Default()
 
     consulClient, err := consulapi.NewClient(&consulapi.Config{Address: consulAddress})
